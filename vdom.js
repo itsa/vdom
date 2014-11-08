582d1a22c1b5fa60cd860ff49fc71374342f6d06
Marco Asbreuk <marco@itsasbreuk.nl>
2014-11-08 19:03:37 +0100

    intermediate saving

--- vdom.js
+++ vdom.js
@@ -4,11 +4,9 @@ module.exports = function (window) {
     var DOCUMENT = window.document,
         VElementClass;
     if (DOCUMENT.doctype.name==='html') {
-        require('./lib/extend-window.js')(window);
         require('./lib/extend-document.js')(window);
-        VElementClass = require('./lib/v-element.js')(window);
         require('./lib/extend-element.js')(window);
         // now parsing and virtualize the complete DOM:
-        require('./lib/node-parser.js')(window)(DOCUMENT._documentElement, VElementClass);
+        require('./lib/node-parser.js')(window)(DOCUMENT._documentElement);
     }
 };
